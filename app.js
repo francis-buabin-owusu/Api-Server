108f3234fa47695e4123c0f09c27f456cbc7d1d4
CyrilBaah <cyril.baah@amalitech.org>
2021-11-05 09:20:53 +0000

    Express api server

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.json({ message: "API is Online!" });
+});
+
+countries = [
+  {
+    id: 1,
+    name: "Ghana",
+  },
+  {
+    id: 2,
+    name: "France",
+  },
+  {
+    id: 3,
+    name: "Scotland",
+  },
+];
+
+names = [
+  {
+    name: "Harry Potter",
+    city: "London",
+  },
+  {
+    name: "Don Quixote",
+    city: "Madrid",
+  },
+  {
+    name: "Joan of Arc",
+    city: "Paris",
+  },
+  {
+    name: "Rosa Park",
+    city: "Alabama",
+  },
+];
+
+todos = [
+  {
+    title: "Clean the kitchen",
+    description:
+      "Mop the floor, wipe the countertop and don't forget to take out the trash!",
+  },
+  {
+    title: "Call Mom",
+    description: "It's her birthday!",
+  },
+  {
+    title: "Water flowers",
+    description: "They need water, or they will die.",
+  },
+];
+
+app.get("/countries", (req, res) => {
+  res.json({ data: countries });
+});
+
+app.get("/names", (req, res) => {
+  res.json({ data: names });
+});
+
+app.get("/todos", (req, res) => {
+    res.json({ data: todos });
+  });
+
+app.get;
+app.listen(port);
+console.log("Listening on port " + port);
+
